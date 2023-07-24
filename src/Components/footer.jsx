976947b66a69cac1b62825bf908a5ad9fc70da33
Farhan Ahmed <89826402+faniAhmed@users.noreply.github.com>
2023-07-24 21:53:15 +0500

    Delete src/Components directory

--- src/Components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./footer.css"
-function Footer(){
-    return (
-    <div className="foot">
-        <h3 className="me">Created by Susan Shilbi</h3>
-    </div>
-    )
-}
-export default Footer;
